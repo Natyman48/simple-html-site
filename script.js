ecb64d4081e961e4c573f5f3f88c7941ff8156a5
Natyman48 <82258514+Natyman48@users.noreply.github.com>
2023-04-30 21:56:11 +0300

    Draft code needs debug
    All changes are made according to instructions. 
    #Code needs debug
    #UI needs improvement
    #needs search button etc...

--- script.js
+++ script.js
@@ -6,6 +6,36 @@ document.getElementById('search-input').addEventListener('keyup', async (e) => {
     // Things to look out for
     // ---
     // Use es6
-    // Error handling
 
-})
+    const res = await fetch('http://localhost:8000/')
+    const json = await res.json()
+
+    const result = `<li>${json.join('</li><li>')}</li>`
+    document.getElementById('results').innerHTML = result
+}
+    var xhr = new XMLHttpRequest()
+    xhr.onload = function(){
+        //handle response
+    };
+    xhr.onerror = function(){
+        //handle error
+    };
+    xhr.open('POST','https://jsonplaceholder.typicode.com/comments?postId=3',true);
+    xhr.setRequestHeader('name: string','application.json');
+
+    var data = JSON.stringify({});  //provide data to send to PHP file
+    xhr.send(data);
+
+    // User input keywords
+    const userKeyword = ['user']
+
+    // API response data containing the keyword properties
+    const apiData = {id: 1. keyword: ['user']}
+
+    // Filter API response data based on the user's input keywords
+    const filteredData = apiData.filter(item=>{// Compare each keyword in the user input with the current item's keywords
+    const matchingKeywords = userKeywords.filter(user=>{return item.JSONkeyword.includes(user);
+    });
+return matchingKeywords.length === userKeywords.length;
+});
+console.console.log(filteredData); 
